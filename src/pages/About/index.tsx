e73030b3067b8d09d81107da3a982249d00f73cb
Ahsan Saeed <ahsan02@gmail.com>
2024-02-22 16:22:56 -0500

    update about section

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,7 +5,7 @@ function About() {
     <Navbar currentLocation="/about" />
       <div>
         <h1 className="text-3xl font-bold">About</h1>
-        <p>I'm currently a third-year student studying at the University of Toronto, where I'm majoring in computer science with a focus in software engineering.</p>
+        <p>I'm currently a fourth-year student studying at the University of Toronto, where I'm majoring in computer science with a focus in software engineering.</p>
       </div>
   </>
 };
