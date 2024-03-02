ae90c4a05ef63f8c870754d064fd377280518d61
saeedahsan <ahsan02@gmail.com>
2024-03-02 14:33:21 -0500

    bug fixes

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/Navbar";
 function About() {
   return <>
     <Navbar currentLocation="/about" />
-      <div>
+      <div className="px-4">
         <h1 className="text-3xl font-bold">About</h1>
         <p>I'm currently a fourth-year student studying at the University of Toronto, where I'm majoring in computer science with a focus in software engineering.</p>
       </div>
